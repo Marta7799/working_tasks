bd4bce87b7ffcc9596a4266b4a1107a10e8da041
Mart <marta.nikiel@yahoo.com>
2023-07-22 11:32:29 +0200

    (examples1.2)

---
+++ js/index.js
@@ -0,0 +1,183 @@
+// "use strict";
+
+// const a = 5;
+// const b = 0;
+// const result = a / b;
+// console.log(result)
+
+// const TOTAL_NUMBER_OF_PHOTOS = 5;
+
+// const firstname = 'Tomasz';
+// const age = 38;
+// const isOldEnoughToDrive = true;
+// const residence = null;
+
+// const a;
+
+// let userAge = 32
+// let userName;
+// let userAge = 44
+// userAge = 44
+
+// console.log(userAge, userName, residence)
+
+// const age = 'abba'
+// const a = 0;
+// const password = 0;
+// const c = 0;
+// const totalNumberOfPhotos = 0;
+// const c2 = 0;
+// const const = 3;
+// let if = 5;
+// const true = false;
+
+// console.log({firstname, age, isOldEnoughToDrive})
+
+// const message = 'Łukasz';
+// console.log("My name is: ", message)
+// alert(message)
+
+// const isComing = confirm('Please confirm your reservation')
+// console.log(isComing)
+
+// const hotelName = prompt('Please enter hotel name')
+// console.log(hotelName)
+
+// const first = 5;
+// const second = "5";
+// const notANumber = "5.3px12"
+
+//const message = "Mango";
+//const secondMessage = "is";
+//const thirdMessage = "happy";
+
+//const someString = `This is my message ${message}`;
+//const someOtherString = "This is my message" + " " + message;
+
+//let score = 23;
+//let result = "";
+//if (score >= 10 && score < 20) {
+// result = "2";
+//} else if (score >= 20 && score < 30) {
+//result = "3";
+//} else if (score >= 30 && score < 40) {
+//if (score > 35) {
+//result = "4.5";
+//} else {
+//result = "4";
+//}
+//} else if (score >= 40) {
+//result = "5";
+//} else {
+//result = "1";
+//}
+// let type;
+// const age = 20;
+
+// if (age >= 18) {
+//     // var a = 5;
+//     type = "adult"
+// } else {
+//     type = "child"
+// }
+
+// // console.log(a)
+
+// const isMonday = true
+// const isBoliwia = false
+// const someValue = undefined;
+// const defaultValue = 2;
+
+// const isAdult = age >= 18 && isMonday || isBoliwia
+// // const someProp = isMonday && 5;
+
+// const resultValue = someValue || defaultValue
+
+// const newType = isAdult ? "adult" : "child"
+
+// const newType = isAdult ? "adult" : isMonday ? "still adult" : "child"
+
+//const outerValue = 2;
+//let globalScope;
+
+//if (true) {
+//  let blockScopeA = 4;
+
+//  if (true) {
+//    const blockScopeB = 3;
+//    globalScope = 6;
+//    console.log(blockScopeA);
+//   console.log({ innerValue, outerValue, moreInnerValue });
+// }
+
+// console.log(blockScopeA);
+// console.log({ innerValue, outerValue, moreInnerValue });
+//}
+
+//if (true) {
+// let innerValue = 14;
+// const blockScopeC = "abba";
+// console.log(blockScopeA);
+
+// const someProp = 5;
+//  console.log(someProp);
+
+//  const otherProp = 11;
+// console.log(someProp, otherProp);
+//}
+
+//console.log(blockScopeA);
+//console.log({ innerValue, outerValue, moreInnerValue });
+
+//let lap = 20;
+
+//while (lap >= 10) {
+// console.log(lap);
+// //lap--;
+// lap -= 2;
+//}
+// let lap = 5;
+// let doWhileLap = 5;
+// let shouldKeepGoing = false
+// let shouldKeepGoingDoWhile = false
+
+// while (shouldKeepGoing) {
+//     console.log(lap)
+
+//     if (lap === 3) {
+//         shouldKeepGoing = false;
+//     }
+
+//     lap--
+// }
+
+// do {
+//     console.log(doWhileLap)
+
+//     if (doWhileLap === 3) {
+//         shouldKeepGoingDoWhile = false;
+//     }
+
+//     doWhileLap--
+// } while (shouldKeepGoingDoWhile)
+
+//for (let lap = 5; lap !== 3; lap--) {
+//  console.log(lap);
+//}
+//const zoomParticipantNumber = 33;
+
+//for (let i = 1; i < 33; i += 2) {
+// const a = 5;
+
+// if (i === 13) {
+//   const b = 4;
+//   console.log("UPS");
+//   break; // i = 33
+// }
+
+//  if (i % 3 !== 0) {
+//   continue;
+// }
+
+// console.log(`Hello only for odd numbers: ${i}`);
+//}
