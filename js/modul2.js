ad6bedbddf05a29407efedf5b9faa8c51ffbf5b4
Mart <marta.nikiel@yahoo.com>
2023-07-25 14:05:23 +0200

    added classes 3

---
+++ js/modul2.js
@@ -0,0 +1,142 @@
+//const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//numbers[4] = "abba";
+//numbers[7] = "fruit";
+//numbers[10] = 123;
+//numbers[11] = 1;
+
+//numbers.push(5);
+//numbers.pop();
+//numbers.pop();
+
+//for (let i = 0; i < numbers.length; i++) {
+//console.log(i)
+//console.log({ i, number: numbers[i] });
+//}
+//const lastElementIndex = numbers.length - 1;
+
+//console.log(numbers[lastElementIndex]);
+
+//const first = [1, 2, 3, [4, 5, 6, [7, 8, 9]]];
+
+//console.log(first);
+
+//const latLangs = [[14, 27],[88, 34],[22, 15],[44, 22],];
+
+//for (const number of numbers) {
+// console.log(number);
+//}
+
+//const someString = "goit2023";
+
+//for (const char of someString) {
+// console.log(char);
+//}
+
+//const colors = ["Red", "Blue", "Green", "White", "Yellow", "Purple"];
+//const colorToFind = "Yellow";
+
+//for (const color of colors) {//
+//if (color === colorToFind) {
+///break;
+//  continue;
+//}
+
+// console.log("Searching");
+//}
+
+//const numberToDivide = 3;
+
+//for (const number of numbers) {
+// if (number % numberToDivide === 0) {
+//   console.log(number);
+//   continue;
+// }
+
+// console.log(`Number ${number} is not divided by ${numberToDivide}`);
+//}
+
+// const array = []
+
+// for (let i = 0; i < 1000; i++) {
+//     array.push(i)
+// }
+
+//const withoutSpace = "Mango";
+//const withSpace = "Mango is good but not so good";
+//const arrayNames = ["Mango", "is", "good", "but", "not", "so", "good"];
+//const fruit = "Banana";
+//const numbersNew = [1, 2, 3, 4, 5];
+
+//console.log(withoutSpace.split(""));
+//console.log(withSpace.split(" "));
+
+// console.log(arrayNames.join("TEST"))
+
+// console.log(arrayNames.indexOf("good"))
+// console.log(arrayNames.includes("good"))
+
+// console.log(arrayNames.indexOf("abba"))
+// console.log(arrayNames.includes("abba"))
+
+// if (arrayNames.indexOf('Mango') > -1) {
+
+// }
+
+// if (fruit === 'Apple' || fruit === 'Mango' || fruit === 'Cherry') {
+//     console.log('it is!')
+// }
+
+// const fruits = ['Apple', 'Mango', 'Cherry'];
+// const hasFruit = fruits.includes(fruit)
+
+// if (hasFruit) {
+//     console.log (`${fruit} is a fruit`)
+// }
+
+// numbers.slice()
+
+//const numbers = [];
+
+//numbers.push(1);
+//console.log(numbers);
+
+//numbers.push(2);
+//console.log(numbers);
+
+// numbers.push(3, 4, 5, 6, 7)
+// console.log(numbers)
+
+// numbers.unshift(0)
+// console.log(numbers)
+
+// numbers.pop()
+// console.log(numbers)
+
+// console.log(numbers.pop())
+// console.log(numbers)
+
+// numbers.shift()
+// console.log(numbers)
+
+// numbers.splice(0, 1)
+// console.log(numbers)
+
+// const firstNumbers = [1, 3, 5, 7, 88]
+// const secondNumbers = [2, 4, 6, 77, 1, 3, 5]
+
+// const firstThenSecond = firstNumbers.concat(secondNumbers)
+// const secondThenFirst = secondNumbers.concat(firstNumbers)
+
+// console.log(firstThenSecond)
+// console.log(secondThenFirst)
+// console.log(firstNumbers)
+// console.log(secondNumbers)
+
+//let some = [5];
+//const arrayWithSome = [1, 2, some, 4];
+// reference
+
+//let some2 = 5;
+//const arrayWithSome2 = [1, 2, [some], 4];
+// copy
