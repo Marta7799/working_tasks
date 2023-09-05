ef807e9f6548201dafd56bd8baa72264a7c23b6d
Mart <marta.nikiel@yahoo.com>
2023-09-05 09:43:38 +0200

    added 7th modul

---
+++ js/modul7.js
@@ -0,0 +1,79 @@
+// const input = document.querySelector('.text-input')
+
+// const handleInput = (event) => {
+//   const currentTarget = event.currentTarget;
+//   console.log(currentTarget.textContent)
+//   console.log(currentTarget.content)
+//   console.log(currentTarget.value)
+//   console.log(currentTarget.text)
+// }
+
+// input.addEventListener('input', handleInput)
+
+// const button = document.querySelector(".goit");
+// const body = document.querySelector("body");
+
+// // console.log(button)
+// // console.log(body)
+
+// const handleClickButton = (event) => {
+//   console.log("button!");
+// };
+
+// const handleClickBody = (event) => {
+//   console.log("body!");
+// };
+
+// body.addEventListener("click", handleClickBody);
+// button.addEventListener("click", handleClickButton);
+
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+const descendant = document.querySelector(".descendant");
+
+const handleClick = (event, name) => {
+  //console.log(name);
+  console.log({
+    name,
+    target: event.target, // gdzie zostało kliknięte
+    currentTarget: event.currentTarget, // jaki element obsługuje ten event
+  });
+};
+
+parent.addEventListener("click", (event) => handleClick(event, "parent"));
+child.addEventListener("click", (event) => handleClick(event, "child"));
+descendant.addEventListener("click", (event) =>
+  handleClick(event, "descendant")
+);
+
+const desc = "descendant :)";
+
+const handleClickDesc = (event) => {
+  event.stopPropagation();
+  event.stopImmediatePropagation();
+  console.log(desc);
+  console.log("descendant! :)");
+};
+
+descendant.addEventListener("click", (event) =>
+  handleClick(event, "descendant")
+);
+descendant.addEventListener("click", (event) =>
+  handleClick(event, "descendant")
+);
+descendant.addEventListener("click", handleClickDesc);
+descendant.addEventListener("click", (event) =>
+  handleClick(event, "descendant")
+);
+descendant.addEventListener("click", (event) =>
+  handleClick(event, "descendant")
+);
+descendant.addEventListener("click", (event) =>
+  handleClick(event, "descendant")
+);
+descendant.addEventListener("click", (event) =>
+  handleClick(event, "descendant")
+);
+descendant.addEventListener("click", (event) =>
+  handleClick(event, "descendant")
+);
