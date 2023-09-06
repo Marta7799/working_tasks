607ab15a60e030675062cce2da70c337f19e37be
Mart <marta.nikiel@yahoo.com>
2023-09-06 12:36:42 +0200

    added _.throttle sample

--- js/samples.js
+++ js/samples.js
@@ -3,24 +3,33 @@
 const output = document.querySelector(".output");
 let scrollEventCounter = 0;
 
-// document.addEventListener("scroll", () => {
-//   scrollEventCounter += 1;
+//1 sposób//
+// document.addEventListener("wheel", (e) => {
+//   console.log(e.wheelDelta);
+//   if (e.wheelDelta < 0) {
+//     scrollEventCounter += 1;
+//   } else {
+//     scrollEventCounter -= 1;
+//   }
 //   output.textContent = scrollEventCounter;
-//   //output.reset();
-//   //output.scroll = null;
 // });
-document.addEventListener("wheel", (e) => {
-  console.log(e.wheelDelta);
-  if (e.wheelDelta < 0) {
-    scrollEventCounter += 1;
-  } else {
-    scrollEventCounter -= 1;
-  }
+// document.addEventListener("click", clickPage);
+// function clickPage(event) {
+//   if (event.target.nodeName !== "BUTTON") {
+//     output.reset();
+//   }
+// }
+
+//2 sposób//
+document.addEventListener("scroll", () => {
+  scrollEventCounter += 1;
   output.textContent = scrollEventCounter;
 });
-document.addEventListener("click", clickPage);
-function clickPage(event) {
-  if (event.target.nodeName !== "BUTTON") {
-    output.reset();
-  }
-}
+
+document.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    scrollEventCounter.throttled += 1;
+    throttledOutput.textContent = scrollEventCounter.throttled;
+  }, 300)
+);
