7faec7ecd21373838e650df9b095b60ed37dd3ce
Mart <marta.nikiel@yahoo.com>
2023-09-07 10:35:00 +0200

    added 14th classes

--- js/modul7.js
+++ js/modul7.js
@@ -27,53 +27,85 @@
 // body.addEventListener("click", handleClickBody);
 // button.addEventListener("click", handleClickButton);
 
-const parent = document.querySelector(".parent");
-const child = document.querySelector(".child");
-const descendant = document.querySelector(".descendant");
+// const parent = document.querySelector(".parent");
+// const child = document.querySelector(".child");
+// const descendant = document.querySelector(".descendant");
 
-const handleClick = (event, name) => {
-  //console.log(name);
-  console.log({
-    name,
-    target: event.target, // gdzie zostało kliknięte
-    currentTarget: event.currentTarget, // jaki element obsługuje ten event
-  });
-};
+// const handleClick = (event, name) => {
+//   //console.log(name);
+//   console.log({
+//     name,
+//     target: event.target, // gdzie zostało kliknięte
+//     currentTarget: event.currentTarget, // jaki element obsługuje ten event
+//   });
+// };
 
-parent.addEventListener("click", (event) => handleClick(event, "parent"));
-child.addEventListener("click", (event) => handleClick(event, "child"));
-descendant.addEventListener("click", (event) =>
-  handleClick(event, "descendant")
+// parent.addEventListener("click", (event) => handleClick(event, "parent"));
+// child.addEventListener("click", (event) => handleClick(event, "child"));
+// descendant.addEventListener("click", (event) =>
+//   handleClick(event, "descendant")
+// );
+
+// const desc = "descendant :)";
+
+// const handleClickDesc = (event) => {
+//   event.stopPropagation();
+//   event.stopImmediatePropagation();
+//   console.log(desc);
+//   console.log("descendant! :)");
+// };
+
+// descendant.addEventListener("click", (event) =>
+//   handleClick(event, "descendant")
+// );
+// descendant.addEventListener("click", (event) =>
+//   handleClick(event, "descendant")
+// );
+// descendant.addEventListener("click", handleClickDesc);
+// descendant.addEventListener("click", (event) =>
+//   handleClick(event, "descendant")
+// );
+// descendant.addEventListener("click", (event) =>
+//   handleClick(event, "descendant")
+// );
+// descendant.addEventListener("click", (event) =>
+//   handleClick(event, "descendant")
+// );
+// descendant.addEventListener("click", (event) =>
+//   handleClick(event, "descendant")
+// );
+// descendant.addEventListener("click", (event) =>
+//   handleClick(event, "descendant")
+// );
+
+// const first = document.querySelector('.first')
+// const second = document.querySelector('.second')
+
+// first.addEventListener("click", event => {
+//   console.log(event.currentTarget)
+// })
+
+// second.addEventListener('click', event => {
+//   console.log(event.target)
+// })
+
+document.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    console.log("scrolled - throttle");
+  }, 300)
 );
 
-const desc = "descendant :)";
-
-const handleClickDesc = (event) => {
-  event.stopPropagation();
-  event.stopImmediatePropagation();
-  console.log(desc);
-  console.log("descendant! :)");
-};
-
-descendant.addEventListener("click", (event) =>
-  handleClick(event, "descendant")
-);
-descendant.addEventListener("click", (event) =>
-  handleClick(event, "descendant")
-);
-descendant.addEventListener("click", handleClickDesc);
-descendant.addEventListener("click", (event) =>
-  handleClick(event, "descendant")
-);
-descendant.addEventListener("click", (event) =>
-  handleClick(event, "descendant")
-);
-descendant.addEventListener("click", (event) =>
-  handleClick(event, "descendant")
-);
-descendant.addEventListener("click", (event) =>
-  handleClick(event, "descendant")
-);
-descendant.addEventListener("click", (event) =>
-  handleClick(event, "descendant")
+document.addEventListener(
+  "scroll",
+  _.debounce(
+    () => {
+      console.log("scrolled - debounce");
+    },
+    300,
+    {
+      leading: true,
+      trailing: true,
+    }
+  )
 );
